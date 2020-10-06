5a64ec0e95e10ce1955ea311f4654c99a92e5dc4
nejraa <67946389+nejraa@users.noreply.github.com>
2020-10-06 20:18:22 +0200

    Finalized.

---
+++ ESN100/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ESN100
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.axAcroPDF1 = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(590, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse ...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(542, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "C:\\ElregPDFViewer\\ESN100\\ESN100_Echogram_PDF_files";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(701, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // axAcroPDF1
+            // 
+            this.axAcroPDF1.Enabled = true;
+            this.axAcroPDF1.Location = new System.Drawing.Point(22, 64);
+            this.axAcroPDF1.Name = "axAcroPDF1";
+            this.axAcroPDF1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axAcroPDF1.OcxState")));
+            this.axAcroPDF1.Size = new System.Drawing.Size(754, 424);
+            this.axAcroPDF1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.axAcroPDF1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ESN100";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.axAcroPDF1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button2;
+        private AxAcroPDFLib.AxAcroPDF axAcroPDF1;
+    }
+}
+
---
+++ ESN100/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using itextsharp.pdfa;
+using iTextSharp.text;
+
+namespace ESN100
+{
+	public partial class Form1 : Form
+	{
+
+		private FileInfo[] files;
+		private FileInfo[] filesRotated;
+		private DirectoryInfo dir;
+		private DirectoryInfo dirRotated;
+		private string MERGEDPDFFILESPATH = "";
+		private string PDFFilesPATH = "";
+		private string PATHROTATED;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		private void button1_Click(object sender, EventArgs e)
+		{
+			FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+			folderDlg.ShowNewFolderButton = true;
+
+			DialogResult result = folderDlg.ShowDialog();
+			if (result == DialogResult.OK)
+			{
+				textBox1.Text = folderDlg.SelectedPath;
+				Environment.SpecialFolder root = folderDlg.RootFolder;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			bool validation = true;
+			PDFFilesPATH = textBox1.Text;
+			PATHROTATED = PDFFilesPATH + "Rotated\\";
+			try
+			{
+				if (!Directory.Exists(PATHROTATED))
+				{
+					DirectoryInfo di = Directory.CreateDirectory(PATHROTATED);
+				}
+			}
+			catch (IOException ioex)
+			{
+				validation = false;
+			}
+			MERGEDPDFFILESPATH = PDFFilesPATH + "Merged\\";
+			try
+			{
+				if (!Directory.Exists(MERGEDPDFFILESPATH))
+				{
+					DirectoryInfo di = Directory.CreateDirectory(MERGEDPDFFILESPATH);
+				}
+			}
+			catch (IOException ioex)
+			{
+				validation = false;
+			}
+
+			if (validateUserEntry(PDFFilesPATH))
+			{
+				validation = rotatein90DegreesAllfiles();
+				if (validation)
+				{
+					validation = mergeAndShowAllPDFFiles();
+				}
+			}
+		}
+
+		private bool validateUserEntry(string FolderPath)
+		{
+			// Checks the value of the text.
+			if (FolderPath.Length == 0)
+			{
+				string message = "You did not provide valid folder path.";
+				string caption = "Folder path error";
+				MessageBoxButtons buttons = MessageBoxButtons.OK;
+				DialogResult result;
+				result = MessageBox.Show(message, caption, buttons);
+				if (result == System.Windows.Forms.DialogResult.OK)
+				{
+					return false;
+				}
+			}
+			else
+			{
+				dir = new DirectoryInfo(FolderPath);
+				files = dir.GetFiles("*.pdf");
+				int fileNum = files.Length;
+
+				if (fileNum == 0)
+				{
+					string message = string.Concat("No PDF files in selected folder.");
+					string caption = "PDF Files";
+					MessageBoxButtons buttons = MessageBoxButtons.OK;
+					DialogResult result;
+					result = MessageBox.Show(message, caption, buttons);
+					if (result == System.Windows.Forms.DialogResult.OK)
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+
+		bool rotatein90DegreesAllfiles()
+		{
+			int rotateInDegree = -90;
+			String myFile;
+			int pagesCount;
+
+			try
+			{
+				for (int i = 0; i < files.Length; i++)
+				{
+					myFile = files[i].FullName;
+					PdfReader reader = new PdfReader(myFile);
+
+					pagesCount = reader.NumberOfPages;
+					for (int n = 1; n <= pagesCount; n++)
+					{
+						PdfDictionary page = reader.GetPageN(1);
+						PdfNumber rotate = page.GetAsNumber(PdfName.ROTATE);
+						page.Put(PdfName.ROTATE, new PdfNumber(rotateInDegree));
+					}
+
+					string filePath = PATHROTATED + files[i].Name;
+					FileStream fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+					PdfStamper stamper = new PdfStamper(reader, fs);
+					stamper.Close();
+					reader.Close();
+				}
+				return true;
+			}
+			catch (Exception e)
+			{
+				return false;
+			}
+		}
+
+		bool mergeAndShowAllPDFFiles()
+		{
+			bool validation;
+			dirRotated = new DirectoryInfo(PATHROTATED);
+			filesRotated = dirRotated.GetFiles("*.pdf");
+			int fileNum = filesRotated.Length;
+
+			// merge files
+			string pathMerged = MERGEDPDFFILESPATH + "mergedAllFiles.pdf"; 
+			//string pathMerged = "C:/ElregPDFViewer/ESN100/ESN100_Echogram_PDF_filesMerged/testFileNejra.pdf";
+			Document document = new Document();
+			PdfImportedPage page = null;
+			PdfReader reader = null;
+			FileStream fos;
+			try
+			{
+				fos = new FileStream(pathMerged, FileMode.Create, FileAccess.Write, FileShare.None);
+			}
+			catch (FileNotFoundException e)
+			{
+				return false;
+			}
+			
+			try
+			{
+				PdfCopy pdf = new PdfCopy(document, fos);
+				document.Open();
+				for (int ii = 0; ii < filesRotated.Length; ii++)
+				{
+					reader = new PdfReader(filesRotated[ii].FullName);
+					for (int i = 0; i < reader.NumberOfPages; i++)
+					{
+						page = pdf.GetImportedPage(reader, i + 1);
+						pdf.AddPage(page);
+					}
+					pdf.FreeReader(reader);
+					reader.Close();
+				}
+				return true;
+			}
+			catch (Exception)
+			{
+				if (reader != null)
+				{
+					reader.Close();
+				}
+				return false;
+			}
+			finally
+			{
+				if (document != null)
+				{
+					document.Close();
+					axAcroPDF1.LoadFile(pathMerged);
+					axAcroPDF1.setShowToolbar(false); //disable pdf toolbar.
+					axAcroPDF1.Enabled = true;
+				}
+			}
+			return true;
+		}
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+			axAcroPDF1.Width = Screen.PrimaryScreen.WorkingArea.Width - 50;
+			axAcroPDF1.Height = Screen.PrimaryScreen.WorkingArea.Height - 100;
+		}
+		private void Form1_Resize(object sender, EventArgs e)
+		{
+			// axAcroPDF1.Width = Screen.PrimaryScreen.WorkingArea.Width - 50;
+			// axAcroPDF1.Height = Screen.PrimaryScreen.WorkingArea.Height - 100;
+		}
+	}
+}
